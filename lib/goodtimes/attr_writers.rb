4291e5c23009334683ed5c0ffb28bd9ac92b85f2
Jason Harrelson <jason@lookforwardenterprises.com>
2013-01-18 18:22:36 -0600

    Fix issue with Goodtimes module not being defined when attr_writers.rb is required.

--- lib/goodtimes/attr_writers.rb
+++ lib/goodtimes/attr_writers.rb
@@ -1,66 +1,70 @@
-module Goodtimes::AttrWriters
+module Goodtimes
 
-  def self.included( base )
-    base.extend ClassMethods
-  end
+  module AttrWriters
 
-  module ClassMethods
-
-    def date_attr_writer( *args )
-      options = args.extract_options!
-      format = options[:format]
-
-      if format
-        if format.is_a?( Symbol )
-          format_key = format
-          if Date::DATE_FORMATS.has_key?( format_key )
-            format = Date::DATE_FORMATS[format_key]
-          else
-            raise "The format key :#{format_key} is not a defined date format"
-          end
-        end
-      else
-        unless Date::DATE_FORMATS.has_key?( :default )
-          raise 'You must provide a format option or define a :default date format'
-        end
-        format = Date::DATE_FORMATS[:default]
-      end
-
-      args.each do |attr|
-        define_method "#{attr}=" do |date|
-          if date.is_a?( String )
-            date = date.blank? ? nil : Date.strptime( date, format )
-          end
-          write_attribute( attr, date )
-        end
-      end
+    def self.included( base )
+      base.extend ClassMethods
     end
 
-    def time_attr_writer( *args )
-      options = args.extract_options!
-      format = options[:format]
+    module ClassMethods
 
-      if format
-        if format.is_a?( Symbol )
-          format_key = format
-          if Time::DATE_FORMATS.has_key?( format_key )
-            format = Time::DATE_FORMATS[format_key]
-          else
-            raise 'The format key you provided is not a defined date format'
+      def date_attr_writer( *args )
+        options = args.extract_options!
+        format = options[:format]
+
+        if format
+          if format.is_a?( Symbol )
+            format_key = format
+            if Date::DATE_FORMATS.has_key?( format_key )
+              format = Date::DATE_FORMATS[format_key]
+            else
+              raise "The format key :#{format_key} is not a defined date format"
+            end
+          end
+        else
+          unless Date::DATE_FORMATS.has_key?( :default )
+            raise 'You must provide a format option or define a :default date format'
+          end
+          format = Date::DATE_FORMATS[:default]
+        end
+
+        args.each do |attr|
+          define_method "#{attr}=" do |date|
+            if date.is_a?( String )
+              date = date.blank? ? nil : Date.strptime( date, format )
+            end
+            write_attribute( attr, date )
           end
         end
-      else
-        unless Time::DATE_FORMATS.has_key?( :default )
-          raise 'You must provide a format option or define a default date format'
-        end
-        format = Time::DATE_FORMATS[:default]
       end
 
-      args.each do |attr|
-        define_method "#{attr}=" do |date|
-          write_attribute( attr, date.blank? ? nil : Time.strptime( date, format ))
+      def time_attr_writer( *args )
+        options = args.extract_options!
+        format = options[:format]
+
+        if format
+          if format.is_a?( Symbol )
+            format_key = format
+            if Time::DATE_FORMATS.has_key?( format_key )
+              format = Time::DATE_FORMATS[format_key]
+            else
+              raise 'The format key you provided is not a defined date format'
+            end
+          end
+        else
+          unless Time::DATE_FORMATS.has_key?( :default )
+            raise 'You must provide a format option or define a default date format'
+          end
+          format = Time::DATE_FORMATS[:default]
+        end
+
+        args.each do |attr|
+          define_method "#{attr}=" do |date|
+            write_attribute( attr, date.blank? ? nil : Time.strptime( date, format ))
+          end
         end
       end
+
     end
 
   end
